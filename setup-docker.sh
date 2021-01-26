510cb4faa9f4b86181982e71566d32bca37397cb
Seri Lee <sally20921@snu.ac.kr>
2021-01-26 22:26:53 +0900

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -4,7 +4,7 @@
 docker build -f noisy.Dockerfile -t sally20921/noisy:1.0.0 .
 
 # before push, login to your docker account using docker login
-sudo docker push sally20921/noisy:1.0.0
+sudo docker push sally20921/swav:2.0.0
 
 # start 
 # docker run --rm -ti --runtime=nvidia --name simclr sally20921/simclr bash
@@ -21,4 +21,4 @@ sudo docker push sally20921/noisy:1.0.0
 # docker ps
 # docker exec -ti container_id bash
 # default shm is 64mb
-docker run --rm --runtime=nvidia --name NoisyStudent --shm-size=2g --gpus=all -v /SSD/ILSVRC2012:/home/data/imagenet -it sally20921/noisy:1.0.0 /bin/bash
+docker run --rm --runtime=nvidia --name BYOL --shm-size=2g --gpus=all -v /SSD/ILSVRC2012:/home/data/imagenet -it sally20921/swav:2.0.0 /bin/zsh
